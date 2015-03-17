a65ef472552fe121844ae74a1b573ffb78a80c08
Ghost1227 <dgriffiths@section214.com>
2015-03-16 22:33:17 -0500

    Fix AJAX support

--- includes/scripts.php
+++ includes/scripts.php
@@ -22,7 +22,7 @@ function edd_password_meter_load_scripts() {
     wp_enqueue_script( 'edd-password-meter-passfield', EDD_PASSWORD_METER_URL . 'assets/js/passfield/js/passfield.js', array( 'jquery' ), EDD_PASSWORD_METER_VER );
     wp_enqueue_script( 'edd-password-meter-passfield-locales', EDD_PASSWORD_METER_URL . 'assets/js/passfield/js/locales.js', array( 'edd-password-meter-passfield' ), EDD_PASSWORD_METER_VER );
     wp_enqueue_style( 'edd-password-meter-passfield', EDD_PASSWORD_METER_URL . 'assets/js/passfield/css/passfield.css', array(), EDD_PASSWORD_METER_VER );
-    wp_enqueue_script( 'edd-password-meter', EDD_PASSWORD_METER_URL . 'assets/js/edd-password-meter.js', array( 'edd-password-meter-passfield' ), EDD_PASSWORD_METER_VER );
+    wp_enqueue_script( 'edd-password-meter', EDD_PASSWORD_METER_URL . 'assets/js/edd-password-meter.js', array( 'edd-password-meter-passfield' ), EDD_PASSWORD_METER_VER, true );
     wp_localize_script( 'edd-password-meter', 'edd_password_meter_vars', array(
         'show_toggle'   => ( edd_get_option( 'edd_password_meter_hide_toggle', false ) ? false : true ),
         'show_generate' => ( edd_get_option( 'edd_password_meter_hide_gen', false ) ? false : true ),
@@ -32,4 +32,26 @@ function edd_password_meter_load_scripts() {
         'match_error'   => __( 'passwords do not match!', 'edd-password-meter' )
     ) );
 }
-add_action( 'edd_after_purchase_form', 'edd_password_meter_load_scripts' );
+add_action( 'wp_enqueue_scripts', 'edd_password_meter_load_scripts' );
+
+
+/**
+ * Add Password Meter on AJAX cart
+ *
+ * @since       1.1.1
+ * @return      void
+ */
+function edd_password_meter_add_to_ajax() {
+    $html = '<script type="text/javascript">
+jQuery(\'#edd_user_pass\').passField({
+    showToggle: ' . ( edd_get_option( 'edd_password_meter_hide_toggle', false ) ? false : true ) . ',
+    showGenerate: ' . ( edd_get_option( 'edd_password_meter_hide_gen', false ) ? false : true ) . ',
+    showTip: ' . ( edd_get_option( 'edd_password_meter_hide_tooltip', false ) ? false : true ) . ',
+    acceptRate: ' . edd_get_option( 'edd_password_meter_strength', 0.8 ) . ',
+    checkmode: ' . edd_get_option( 'edd_password_meter_checkmode', 'STRICT' ) . '
+});
+</script>';
+
+    echo $html;
+}
+add_action( 'edd_after_purchase_form', 'edd_password_meter_add_to_ajax' );
